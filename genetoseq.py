4a52e1d64efc0c7ff58ff62de715de3a7efae383
Bart Edelbroek <bart.edelbroek@icm.uu.se>
2020-03-27 14:32:56 +0100

    added functionality for dictybase gene_IDs

--- genetoseq.py
+++ genetoseq.py
@@ -6,8 +6,19 @@ import re, sys, pybedtools
 #read in gene_id's 
 genelist = open(sys.argv[1]).read().split('\n')
 
-#read in original .bed file
+gfffile = open('./reference/D_discoideum_PASAua.gff').readlines()
 
+def genetotranscript(genelist, gfffile):
+	s = []
+	for line in gfffile:
+		if re.search("mRNA", line):
+			for gene in genelist:
+				if re.search(gene, line):
+					s.append(re.search('(?<=ID=)\w+', line).group())
+	return(s)
+
+
+#read in original .bed file
 oribed = open('./reference/D_discoideum_PASAua.bed').readlines()
 
 #subset of .bed for genelist
@@ -18,9 +29,12 @@ for line in oribed:
 		if re.search(str(gene), line): 
 			newbed+=line 
 
-print(newbed)
+#print(newbed)
 
 bt = pybedtools.BedTool(newbed, from_string=True)
 fastaref = './reference/dicty_2.7.toplevel.masked.fa'
 genseq = bt.sequence(fi=fastaref, s=True)
-print(open(genseq.seqfn).read())
+#print(open(genseq.seqfn).read())
+
+r = genetotranscript(genelist, gfffile)
+print(r)
