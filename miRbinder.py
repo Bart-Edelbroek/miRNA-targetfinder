420652b83de9b86412e63921bfa26f883c933618
Bart Edelbroek <bart.edelbroek@icm.uu.se>
2020-04-02 14:40:34 +0200

    Added plot to average over the output. Evaluates the minimum binding energy of the microRNA over the (normalized) total length of the transcripts. The intervals at which it evaluates the binding energy can be specified with the steps variable. Note: if you specify more steps than the length of the smallest transcript the output will be useless. The program cannot divide the binding energy of your 300 nucleotide transcript into parts of 400 for example. As such, the highest precision plot you will be able to get is by using steps equal to length of smallest transcript in the list.

--- miRbinder.py
+++ miRbinder.py
@@ -7,6 +7,7 @@ from os import system
 
 genelist = sys.argv[1]
 query_in = sys.argv[2]
+combinedplot_steps = 100
 g2seq = Gene2seq(genelist)
 sequencelist = g2seq.run()
 
@@ -17,7 +18,7 @@ subprocess.run(["IntaRNA", "-t", "genesequences.fasta", "-q", query_in,
 	"--seedBP=6", "--seedNoGU", "--seedMinPu=0.001", "--outMinPu=0.001",
 	"--outNoLP", "--out=tMinE:MYPROFILEFILE.csv"])
 
-analysis = Analysis(genelist)
+analysis = Analysis(genelist, combinedplot_steps)
 analysis.run()
 
 subprocess.run(["rm ./MYPROFILEFILE-t*q1.csv"], shell = True)
--- miRbinder/outputanalysis.py
+++ miRbinder/outputanalysis.py
@@ -4,11 +4,13 @@
 import matplotlib.pyplot as plt
 import csv
 import pandas as pd
+import numpy as np
 
 class Analysis(object):
 	"""docstring for Analysis"""
-	def __init__(self, genelist):
+	def __init__(self, genelist, steps):
 		self.genelist = open(genelist).read().split('\n')
+		self.steps = steps
 
 	def plotoverlay(self, genelist):
 		for a in range(1,(len(genelist)+1)):
@@ -26,7 +28,29 @@ class Analysis(object):
 			plt.plot(x,y,label=str(i.columns[1]))
 			plt.legend(loc='center left', bbox_to_anchor=(1.04, 0.5))
 		plt.savefig('fig1.pdf', bbox_inches='tight')
+		plt.clf()
 
+	def plotcombined(self, genelist, steps):
+		comb = np.array([])
+		for a in range(1,(len(genelist)+1)):
+			i = pd.read_csv(('MYPROFILEFILE-t'+str(a)+'q1.csv'), sep=';')
+			x = np.array(i['idx']/len(i['idx']))
+			y = np.array([])
+			for n in i['minE']:
+				if n < 0:
+					y = np.append(y, n)
+				else:
+					y = np.append(y, 0)
+			s = np.array_split(y,int(steps))
+			a = np.array([])
+			for l in s:
+				a = np.append(a, np.average(l))
+			comb = np.append(comb, a)
+		y = (np.mean(np.array_split(comb,len(genelist)), axis=0))
+		x = np.linspace(0,1, int(steps))
+		plt.plot((x),y)
+		plt.savefig('fig2.pdf')
 
 	def run(self):
 		overlayplot = self.plotoverlay(self.genelist)
+		plotcomb = self.plotcombined(self.genelist, self.steps)
